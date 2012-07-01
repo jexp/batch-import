0e473807c197120da90802ae5b3f16a6fed6d383
Max De Marzi <maxdemarzi@hotmail.com>
2012-07-01 00:14:32 -0500

    updating libraries and adding Lucene dependency in order to implement node and relationship indexing.

--- src/main/java/org/neo4j/batchimport/Importer.java
+++ src/main/java/org/neo4j/batchimport/Importer.java
@@ -1,41 +1,59 @@
 package org.neo4j.batchimport;
 
 import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.kernel.impl.batchinsert.BatchInserterImpl;
+import org.neo4j.unsafe.batchinsert.BatchInserter;
+import org.neo4j.unsafe.batchinsert.BatchInserters;
+import org.neo4j.unsafe.batchinsert.BatchInserterImpl;
+import org.neo4j.unsafe.batchinsert.BatchInserterIndexProvider;
+import org.neo4j.unsafe.batchinsert.BatchInserterIndex;
+import org.neo4j.unsafe.batchinsert.LuceneBatchInserterIndexProvider;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
+import org.neo4j.helpers.collection.MapUtil;
 
 import static org.neo4j.helpers.collection.MapUtil.map;
 import static org.neo4j.helpers.collection.MapUtil.stringMap;
+import static org.neo4j.index.impl.lucene.LuceneIndexImplementation.EXACT_CONFIG;
 
 public class Importer {
     private static Report report;
-    private BatchInserterImpl db;
-
+    private BatchInserter db;
+    private BatchInserterIndexProvider lucene;
+    
     public Importer(File graphDb) {
-        final Map<String, String> config = getConfig();
-        db = new BatchInserterImpl(graphDb.getAbsolutePath(), config);
-        report = new Report(10 * 1000 * 1000, 100);
-    }
+    	Map<String, String> config = new HashMap<String, String>();
+    	try {
+	        if (new File("batch.properties").exists()) {
+	        	System.out.println("Using Existing Configuration File");
+	        } else {
+		        System.out.println("Writing Configuration File to batch.properties");
+				FileWriter fw = new FileWriter( "batch.properties" );
+		        fw.append( "neostore.nodestore.db.mapped_memory=100M\n"
+		                 + "neostore.relationshipstore.db.mapped_memory=1G\n"
+		                 + "neostore.propertystore.db.mapped_memory=250M\n"
+		                 + "neostore.propertystore.db.strings.mapped_memory=100M\n"
+		                 + "neostore.propertystore.db.arrays.mapped_memory=0M\n"
+		                 + "neostore.propertystore.db.index.keys.mapped_memory=15M\n"
+		                 + "neostore.propertystore.db.index.mapped_memory=15M" );
+		        fw.close();
+	        }
 
-    private Map<String, String> getConfig() {
-        if (new File("batch.properties").exists()) {
-            return BatchInserterImpl.loadProperties("batch.properties");
-        } else {
-            return stringMap(
-                    "dump_configuration", "true",
-                    "cache_type", "none",
-                    "neostore.propertystore.db.index.keys.mapped_memory", "5M",
-                    "neostore.propertystore.db.index.mapped_memory", "5M",
-                    "neostore.nodestore.db.mapped_memory", "50M",
-                    "neostore.relationshipstore.db.mapped_memory", "250M",
-                    "neostore.propertystore.db.mapped_memory", "200M",
-                    "neostore.propertystore.db.strings.mapped_memory", "100M");
+        config = MapUtil.load( new File(
+                "batch.properties" ) );
+
+        } catch (Exception e) {
+    		System.out.println(e.getMessage());
         }
+                
+        db = BatchInserters.inserter(graphDb.getAbsolutePath(), config);
+        lucene = new LuceneBatchInserterIndexProvider(db);
+        report = new Report(10 * 1000 * 1000, 100);
     }
 
     public static void main(String[] args) throws IOException {
@@ -45,17 +63,33 @@ public class Importer {
         File graphDb = new File(args[0]);
         File nodesFile = new File(args[1]);
         File relationshipsFile = new File(args[2]);
+        File indexFile = new File("");
+        String indexName = "";
+        String indexType = "";
+        
         if (!graphDb.exists()) graphDb.mkdirs();
         Importer importBatch = new Importer(graphDb);
         try {
             if (nodesFile.exists()) importBatch.importNodes(nodesFile);
-            if (relationshipsFile.exists()) importBatch.importRelationships(relationshipsFile);
-        } finally {
+            if (relationshipsFile.exists()) importBatch.importRelationships(relationshipsFile);         
+			for (int i = 3; i < args.length; i = i + 4) {
+				indexName = args[i+1];
+				indexType = args[i+2];
+				indexFile = new File(args[i + 3]);
+				if (args[i].equals("node_index")) {
+					if (indexFile.exists()) importBatch.importNodeIndexes(indexFile, indexName, indexType);
+				} else {
+					if (indexFile.exists()) importBatch.importRelationshipIndexes(indexFile, indexName, indexType);
+				}
+			
+			}
+		} finally {
             importBatch.finish();
         }
     }
 
     private void finish() {
+        lucene.shutdown();
         db.shutdown();
         report.finish();
     }
@@ -148,6 +182,54 @@ public class Importer {
         }
         report.finishImport("Relationships");
     }
+
+    private void importNodeIndexes(File file, String indexName, String indexType) throws IOException {
+    	BatchInserterIndex index;
+    	if (indexType.equals("fulltext")) {
+    		index = lucene.nodeIndex( indexName, stringMap( "type", "fulltext" ) );
+    	} else {
+    		index = lucene.nodeIndex( indexName, EXACT_CONFIG );
+    	}
+        
+        BufferedReader bf = new BufferedReader(new FileReader(file));
+        
+        final Data data = new Data(bf.readLine(), "\t", 1);
+        Object[] node = new Object[1];
+        String line;
+        report.reset();
+        while ((line = bf.readLine()) != null) {        
+            final Map<String, Object> properties = map(data.update(line, node));
+            index.add(id(node[0]), properties);
+            report.dots();
+        }
+                
+        report.finishImport("Nodes into " + indexName + " Index");
+    }
+
+    private void importRelationshipIndexes(File file, String indexName, String indexType) throws IOException {
+    	BatchInserterIndex index;
+    	if (indexType.equals("fulltext")) {
+    		index = lucene.relationshipIndex( indexName, stringMap( "type", "fulltext" ) );
+    	} else {
+    		index = lucene.relationshipIndex( indexName, EXACT_CONFIG );
+    	}
+
+        BufferedReader bf = new BufferedReader(new FileReader(file));
+        
+        final Data data = new Data(bf.readLine(), "\t", 1);
+        Object[] rel = new Object[1];
+        String line;
+        report.reset();
+        while ((line = bf.readLine()) != null) {        
+            final Map<String, Object> properties = map(data.update(line, rel));
+            index.add(id(rel[0]), properties);
+            report.dots();
+        }
+                
+        report.finishImport("Relationships into " + indexName + " Index");
+
+    }
+
 
     static class Type implements RelationshipType {
         String name;
