f8c849822f12ccae83b5108bd68b696365380356
cescalante-carecloud <cescalante@carecloud.com>
2013-12-03 11:29:09 -0500

    simple fix
    Now nodes are added, still not v 2.0-d

--- sample/import.sh
+++ sample/import.sh
@@ -1,3 +1,3 @@
 echo "Run in main directory sh sample/import.sh"
 mvn test-compile exec:java -Dexec.mainClass="org.neo4j.batchimport.Importer" \
-  -Dexec.args="sample/batch.properties target/graph.db sample/nodes.csv,sample/nodes2.csv sample/rels.csv"
+ -Dexec.args="sample/batch.properties target/graph.db sample/nodes.csv sample/rels.csv"
